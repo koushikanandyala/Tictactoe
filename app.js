48193f385f7bc0518ba2ec71906deab20414ded4
koushikanandyala <koushika1729@gmail.com>
2024-11-04 12:16:41 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,95 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let click_count=0; 
+
+let turnO = true; // playerX , playerO 
+
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+];
+
+const resetGame = () => {
+    turnO = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+    click_count=0; 
+};
+
+
+
+boxes.forEach((box) =>{
+    box.addEventListener("click", () =>{
+        console.log("box was clicked");
+        if(turnO){
+             box.innerText = "O";
+             turnO = false;
+        }else{
+            box.innerText = "X";
+             turnO = true;
+        }
+       box.disabled = true;
+
+       checkWinner();
+    })
+});
+
+const disabledBoxes = () => {
+    for(let box of boxes){
+        box.disabled = true;
+    }
+}
+
+const enableBoxes = () => {
+    for(let box of boxes){
+        box.disabled = false;
+        box.innerText = "";
+    }
+}
+
+const showWinner = (winner) => {
+msg.innerText = `Congratulations, Winner is ${winner} `;
+msgContainer.classList.remove("hide");
+disabledBoxes();
+}
+
+const checkWinner = () => {
+    for(let pattern of winPatterns){
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if(pos1Val != "" && pos2Val != "" && pos3Val != ""){
+            if(pos1Val === pos2Val && pos2Val === pos3Val){
+                console.log("winner", pos1Val)
+                showWinner(pos1Val);
+            }
+        }
+
+       /* if(pos1==pos2 && pos2==pos3){
+            // console.log(`Winner is ${pos1}`)
+            Showwin(pos1);
+            disable();
+        }
+       else if(click==9){
+        msg.innerHTML="Match is draw";
+        msg.classList.remove("hide");
+       }
+     */
+
+
+    }
+}
+
+newGameBtn.addEventListener("click",resetGame);
+resetBtn.addEventListener("click",resetGame);
